8d07beab491b4c3b87cd4f5ca797f325ec3f4cb0
utkpython <utkpython@gmail.com>
2014-10-12 22:31:39 -0400

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,20 @@
+####
+# 1.py
+# erase the hashtags to run the code!
+
+#print 1 + 1 
+
+#print 7.1 * 4000
+
+#a = 9
+#print a**2
+
+#print 100/50
+#print 100/51
+
+#phi = (1 + 5**(1/2.)) / 2
+
+#e = (1 + 1/100000.)**100000
+
+
+
