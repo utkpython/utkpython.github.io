8cccf0fc7c2728dfb931068af6c8abe3beb2df33
utkpython <utkpython@gmail.com>
2014-10-13 12:00:03 -0700

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,12 @@
+# remove the hashtags below to get the code to run
+
+# import math
+
+# math.cos(12)
+# math.sqrt(math.cos(12))
+
+
+# import numpy as np
+# a = [1,2,3,4,5]
+# np.mean(a)
+# np.std(a)
