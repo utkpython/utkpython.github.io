c4a16774fee3ee3f403c03e0230a10a6313cd492
utkpython <utkpython@gmail.com>
2014-10-13 18:07:21 -0400

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,9 @@
+
+
+
+
+#groceries = ['cereal', 'dog food', 'milk', 'eggs']
+#feed_pets = ['cat','dog', 'fish']
+#change_oil = ['porsch', 'golf cart']
+#things_to_do = [groceries, feed_pets, change_oil]
+#print(things_to_do)
