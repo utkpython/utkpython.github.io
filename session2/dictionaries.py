dbb01f8925145554ffceab7a09742e9c308d540f
utkpython <utkpython@gmail.com>
2014-10-27 15:25:13 -0400

    Update dictionaries.py

--- session2/dictionaries.py
+++ session2/dictionaries.py
@@ -2,3 +2,22 @@
 
 #describe["cat"]
 #describe["python"]
+
+#adjectives = { "cat": ["cute","fluffy"], 
+#                "dog": ["friendly","loyal", "happy"], 
+#                "fish": ["smelly","swimmy"], 
+#                "dragon": ["mythical","fire-breathing"], 
+#                "python": ["fun", "badass"] }
+
+#for noun in adjectives:
+#  print "a", ', '.join(adjectives[noun]), noun
+  
+  
+  
+#tetrisScores = { "Kim Kardashian": 100000,
+#                "Kanye West": 80000,
+#                "Taylor Swift": 130000,
+#                "Bill Gates": 97000,
+#                "Ash Ketchum": 900000,
+#                "Barack Obama": 102000,
+#                "Marissa Mayer": 1000000}
