f79c2c5ae1d3d453ee571db4dc4d26ee1656da74
utkpython <utkpython@gmail.com>
2014-10-28 16:59:12 -0400

    Update yahooFinance.py

--- session2/yahooFinance.py
+++ session2/yahooFinance.py
@@ -1,6 +1,18 @@
 from ystockquote import get_historical_prices
 from datetime import date
+from pylab import *
 
-preliminaryData = get_historical_prices('YHOO','2014-01-01','2014-10-20')
+data = get_historical_prices("IBM","2014-01-01","2014-10-20")
 
-#print type(preliminaryData)
+smaller_data = {}
+for dates in data:
+    smaller_data[dates] = data[dates]['Adj Close']
+
+
+dates = smaller_data.keys()
+dates.sort(key=lambda dates: dates)
+
+prices = [smaller_data[date] for date in dates]
+
+plot(prices)
+show(plot)
