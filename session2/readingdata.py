5cc89d4e4ec11ee22df8f35da568b1b4d4d49b98
utkpython <utkpython@gmail.com>
2014-10-28 11:32:50 -0400

    Create readingdata.py

---
+++ session2/readingdata.py
@@ -0,0 +1,25 @@
+#fin = open('words.txt')
+#print fin
+
+#fin.readline()
+#fin.readline()
+
+#line = fin.readline()
+#word = line.strip()
+#print word
+
+#fin = open('words.txt')
+#for line in fin:
+#  word = line.strip()
+#  print word
+
+
+import pandas
+gpa=pandas.read_csv('hw7education.csv')
+gpa["College"]
+
+
+
+
+
+  
