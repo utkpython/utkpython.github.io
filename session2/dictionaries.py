ecc0487b00e62c2988834a4e1a89c5d32402c1f8
utkpython <utkpython@gmail.com>
2014-10-27 23:12:40 -0400

    Update dictionaries.py

--- session2/dictionaries.py
+++ session2/dictionaries.py
@@ -27,10 +27,23 @@
 
 #### a more complicated example
 #### didn't know that bill gates' birthday was actually 10/28 --- the day of the seminar
-#tetrisDB = { "Kim Kardashian":   {"high score":100000, "age": 34},
-#                "Kanye West":    {"high score": 80000, "age": 37},
-#                "Taylor Swift":  {"high score": 130000, "age": 24},
+#tetrisDB = { "Kim Kardashian":   {"high score":300000, "age": 34},
+#                "Kanye West":    {"high score": 200000, "age": 37},
+#                "Taylor Swift":  {"high score": 430000, "age": 24},
 #                "Bill Gates":    {"high score": 97000, "age": 60},
-#                "Ash Ketchum":   {"high score": 900000, "age": 10},
+#                "Ash Ketchum":   {"high score": 800000, "age": 10},
 #                "Barack Obama":  {"high score": 102000, "age": 53},
 #                "Marissa Mayer": {"high score": 1000000, "age": 39}}
+
+#names = []
+#scores = []
+#ages = []                               
+#for player in tetrisDB:
+#    names.append(player)
+#    scores.append(tetrisDB[player]["high score"])
+#    ages.append(tetrisDB[player]["age"])
+
+#from pylab import *
+
+#scatter(ages,scores)
+#show(plot)
