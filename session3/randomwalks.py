6f6bed0c5acf2c3656539b80d2574e5ca5b54e20
utkpython <utkpython@gmail.com>
2014-11-04 10:55:35 -0500

    Update randomwalks.py

--- session3/randomwalks.py
+++ session3/randomwalks.py
@@ -31,3 +31,77 @@ for i in xrange(numSteps):
 plt.plot(walk)
 plt.show()
 
+########2d
+positionx = 0
+positiony = 0
+
+walkx = [positionx]
+walky = [positiony]
+
+numSteps = 10000
+plt.show(block=False)
+
+for i in xrange(numSteps):
+    if randint(0,1)==1:
+        stepx = 1         #take a step forward
+    else:
+        stepx = -1        #take a step backward
+    if randint(0,1)==1:
+        stepy = 1         #take a step forward
+    else:
+        stepy = -1        #take a step backward  
+    positionx = positionx + stepx
+    positiony = positiony + stepy
+    walkx.append(positionx)
+    walky.append(positiony)
+    
+
+
+plt.plot(walkx,walky,marker='',color='b')
+
+##########3d
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+
+positionx = 0
+positiony = 0
+positionz = 0
+xs = [positionx]
+ys = [positiony]
+zs = [positionz]
+numSteps = 100
+plt.show(block=False)
+
+for i in xrange(numSteps):
+    if randint(0,1)==1:
+        stepx = 1         #take a step forward
+    else:
+        stepx = -1        #take a step backward
+    if randint(0,1)==1:
+        stepy = 1         #take a step forward
+    else:
+        stepy = -1        #take a step backward 
+    if randint(0,1)==1:
+        stepz = 1         #take a step forward
+    else:
+        stepz = -1        #take a step backward  
+    positionx = positionx + stepx
+    positiony = positiony + stepy
+    positionz = positionz + stepz
+    xs.append(positionx)
+    ys.append(positiony)
+    zs.append(positionz)
+    
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+n = 100
+for c, m, zl, zh in [('r', 'o', -50, -25), ('b', '^', -30, -5)]:
+
+    ax.plot(xs, ys, zs, c=c, marker=m)
+
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+
+plt.show()
