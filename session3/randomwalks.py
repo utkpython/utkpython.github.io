37d315ad342820ead04abf48d89d994c4967d112
utkpython <utkpython@gmail.com>
2014-11-04 10:03:38 -0500

    Update randomwalks.py

--- session3/randomwalks.py
+++ session3/randomwalks.py
@@ -1,6 +1,17 @@
 from random import randint
 import matplotlib.pyplot as plt
 
+"""simple example to get us started"""
+#heads = 0
+#tails = 0
+#for i in range(1, 10001):
+#    if randint(0,1):
+#        heads = heads + 1
+#    else:
+#        tails = tails + 1
+#heads/float(heads+tails)
+
+
 position = 0
 
 walk = [position]
