8ba449f022ab6c05fccf2129e91b53ef467bc709
utkpython <utkpython@gmail.com>
2014-11-03 23:45:13 -0500

    Update review.py

--- session3/review.py
+++ session3/review.py
@@ -5,5 +5,27 @@
 # code do?
 
 def kablam(yourList):
-  if type(yourList) != list:
-    print "can't kablam"
+    '''this is called a docstring.
+    Ideally, it would tell you what this
+    function does'''
+    if type(yourList) != list:
+        print "can't kablam"
+    else:
+        for i in yourList:
+            print i
+
+####################
+####################
+# now what does this 
+# block of code do?
+
+def kazam(yourList):
+  '''this is another magical function'''
+    if type(yourList) != list:
+        print "can't kazam"
+    else:
+        LList = len(yourList)
+        for i in range(1,LList):
+            if yourList[i] > yourList[i-1]:
+                kazam = yourList[i]
+        return kazam
