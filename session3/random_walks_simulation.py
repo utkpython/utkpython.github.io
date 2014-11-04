cd069b03afe2bc356257b826097b184ecefe9756
utkpython <utkpython@gmail.com>
2014-11-04 13:44:59 -0500

    Update random_walks_simulation.py

--- session3/random_walks_simulation.py
+++ session3/random_walks_simulation.py
@@ -1,4 +1,25 @@
-# a simulation will go here
-# we can pose the problem as something like "students getting ahead in life"
+# modeling the idea that "students getting ahead in life"
 # some start off in a higher spot
 # some have more skill, some less
+
+from random import randint
+import matplotlib.pyplot as plt
+
+def rand_walk(numSteps, position, skill):
+    '''returns a random walk list of size numSteps
+       position is the starting position
+       skill is a measure of how far forward you are
+            able to walk'''
+    walk = [position]
+    numSteps = numSteps
+    for i in xrange(numSteps):
+        if randint(0,1):
+            step = 1 * skill       #take a step forward
+        else:
+            step = -1        #take a step backward
+  
+        position = position + step
+  
+        walk.append(position)
+    
+    return walk
