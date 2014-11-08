598df2b52f628a1118a1abde41e05699a531f40b
utkpython <utkpython@gmail.com>
2014-11-08 12:48:12 -0500

    Create modulesLesson.py

---
+++ session4/modulesLesson.py
@@ -0,0 +1,18 @@
+# this will be a brief lesson/review about modules
+# goals: have them create a really simple module
+
+
+#### predict the output of this code
+import re
+print type(re)
+####
+
+
+### dir() is a super useful function for learning
+###     it tells you all of the official attributes
+###     of the module/object
+
+print dir(re)
+
+import numpy
+print dir(numpy) ##whoa
