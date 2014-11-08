045649b4c0d677384655b1335ab650bdc85994c3
biz <bx47@cock.li>
2014-11-08 12:59:00 -0500

    moved session 1 stuff to its own folder

---
+++ session1/1.py
@@ -0,0 +1,35 @@
+####
+# 1.py
+# erase the hashtags to run the code!
+
+#print 1 + 1 
+
+#print 7.1 * 4000
+
+#a = 9
+#print a**2
+
+#print 100/50
+#print 100/51
+
+#phi = (1 + 5**(1/2.)) / 2
+
+#e = (1 + 1/100000.)**100000
+
+#One way to estimate pi is to use the following infinite series
+
+#index = 1
+#pi = 0.
+#pie = 4.
+#while abs(pi-pie) > .00001:
+#    pie = pi
+#    pi = pi + (-1)**(index + 1)/(2.*index-1)
+#    index = index + 1
+#print 4*pi
+
+#Or we can do this
+#import math
+#print math.pi
+
+
+
