183ba54675250f036f0ccc4e4fe15e773130a808
utkpython <utkpython@gmail.com>
2014-11-11 14:24:18 -0500

    Update review.py

--- session4/review.py
+++ session4/review.py
@@ -7,7 +7,21 @@ die_rolls = []
 
 print randint(1,6)
 
-
-
 # review functions
+def func(x,y):
+    if type(x) != int or type(y) != int:
+        return "fail"
+    else:
+        return 1
+
+# what does the following code do? 
+# print type(func) 
+
+#what about this?
+# print type(func(1,100))
+
+#what about this?
+# print type(func(1.0,1))
+
+
 # review dict,list,tuple
