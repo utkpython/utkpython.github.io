e4c34c0a16a303defe3dc2215f4548458d8cc975
utkpython <utkpython@gmail.com>
2014-11-11 14:17:51 -0500

    Update review.py

--- session4/review.py
+++ session4/review.py
@@ -1,10 +1,13 @@
-# review simulation concepts
-
 # "God does not play dice" -Einstein
 # well we play dice here at UTK.py
 # goal... simulate dice rolling
 from random import randint
 
+die_rolls = []
+
 print randint(1,6)
+
+
+
 # review functions
 # review dict,list,tuple
