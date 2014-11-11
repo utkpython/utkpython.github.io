581cf694832208259eda6f18bd497f0df7017c7d
utkpython <utkpython@gmail.com>
2014-11-11 14:48:21 -0500

    Update review.py

--- session4/review.py
+++ session4/review.py
@@ -1,11 +1,16 @@
 # "God does not play dice" -Einstein
 # well we play dice here at UTK.py
-# goal... simulate dice rolling
+
 from random import randint
 
 die_rolls = []
 
 print randint(1,6)
+# simulate 10000 die being rolled
+# can you do it?
+# can you plot the results?
+
+
 
 # review functions
 def func(x,y):
@@ -24,4 +29,33 @@ def func(x,y):
 # print type(func(1.0,1))
 
 
+
+
+
 # review dict,list,tuple
+
+superpowers = { "Spiderman": ["smarts","web-spinning","pizza-cooking"],
+                "Superman": ["strength", "flight", "being-handsome"],
+                "Michelle Obama": ["smiling","world-domination"],
+                "Ken Thompson": ["programming"]
+             }
+
+# write some code to show all of the people in the database             
+# write some code to show how many powers each person has
+
+myList = ["bro", "broski", "dude", "man", "dawg" ]
+
+# what is the length of the list?
+# what is the last element?
+# what does this do?
+for i in myList:
+    print i
+    
+for i in reversed(myList):
+    print i
+    
+    
+# what is the difference between a tuple and a list?
+
+
+
