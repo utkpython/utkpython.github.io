4d6fd6eb3d36dad8d1e90a173c068e0bcdb252ca
biz <bx47@cock.li>
2014-11-17 12:23:56 -0500

    initial commit for session 5

---
+++ session5/fib.py
@@ -0,0 +1,9 @@
+# what are the fibonacci numbers?
+# 1,1,2,3,5,8,...
+# write a program that will give a list of fib numbers
+
+
+# what about the inner-ratios?
+
+# can we generalize the fibonacci numbers?
+# what do we find?
---
+++ session5/puppies.py
@@ -0,0 +1,6 @@
+# list some multiples of 3 or 5
+# list the multiples of 3 or 5 that are less than 14
+# what is the sum of this list?
+
+# can you find the sum of all the multiples of 3 or 5 than are less than 1000?
+
