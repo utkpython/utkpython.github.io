fed56f90006f7e02304ac304fb41179a4a16d0b7
utkpython <utkpython@gmail.com>
2014-11-18 12:15:05 -0500

    Update search.py

--- session5/search.py
+++ session5/search.py
@@ -7,15 +7,26 @@ for line in words:
     word = line.replace('\n','')
     dictionary.append(word)
     
-'ant' in dictionary
-'bug' in dictionary
+'ant' in dictionary  
+'bug' in dictionary   ##how does this work? How is it so fast?
 
 #search
+word = 'ant'
+def slowsearch(word, dic):
+    index = 0
+    for i in dic:
+        index += 1
+        if word == i:
+            return index
+        
+
+
+#smarter search
 dic = dictionary
 t = True
 mid = len(dic)/2
 mids = mid
-word = 'fat'
+word = 'big'
 index = 0
 while t == True:
     if mids%2 == 0:
@@ -23,6 +34,8 @@ while t == True:
     else:
         mids = mids/2 + 1
     index += 1
+    if mid < 0:
+        mid = 0
     if word == dic[mid]:
         print mid
         print dic[mid]
@@ -37,7 +50,7 @@ while t == True:
 
 dic = dictionary
 
-def search(word, list):
+def search(word, dic):
     t = True
     mid = len(dic)/2
     mids = mid
@@ -48,6 +61,8 @@ def search(word, list):
         else:
             mids = mids/2 +1
         index += 1
+        if mid < 0:
+            mid = 0
         if word == dic[mid]:
             return mid , dic[mid]
             t = False
@@ -58,4 +73,44 @@ def search(word, list):
         if index > log(len(dic))/log(2) + 1:
             return "your word is not contained in this dictionary"
             
-            
+#comparing          
+
+import time
+
+start = time.time()
+slowsearch("zymurgy", dic)
+end = time.time()
+print end - start
+
+start = time.time()
+search("zymurgy", dic)
+end = time.time()
+print end - start
+
+####real test
+
+f = open("dict.txt")
+i = 1
+l = list()
+for line in f:
+    stripped = line.rstrip("\r\n\t").lower()
+    if ('\'' in stripped):
+        continue
+    else:
+        l.append(stripped)
+        #print "Adding item " + str(i)
+        i += 1
+
+print "Done, processed " + str(i-1) + " items."
+
+l.sort()
+
+start = time.time()
+slowsearch("zzz", l)
+end = time.time()
+print end - start
+
+start = time.time()
+search("zzz", l)
+end = time.time()
+print end - start
